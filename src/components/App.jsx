5f98b00318ec63da2525f98e0c8df66eec15c650
PatrickSpace <jplosta01@gmail.com>
2022-12-24 21:57:58 -0500

    control

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 class="text-center text-lg shadow bg-sky-600 h-20">Hola crrano</h1>
+  );
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
